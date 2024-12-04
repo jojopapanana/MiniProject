cef7a0f7d04929fcd8c4b400d7b2e5222789bf1b
jojopapanana <jovannamelissa6@gmail.com>
2024-12-04 09:23:08 +0700

    Initial Commit

---
+++ MiniProjectTests/MiniProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniProjectTests.swift
+//  MiniProjectTests
+//
+//  Created by Jovanna Melissa on 04/12/24.
+//
+
+import Testing
+@testable import MiniProject
+
+struct MiniProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
